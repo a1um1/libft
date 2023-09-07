64aecc93ebef04bb37ef5bf4c7f9c28c3f876444
meow <katesan945@gmail.com>
2023-09-07 14:28:19 +0700

    fixed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ml <ml@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: tlakchai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 17:05:14 by tlakchai          #+#    #+#             */
-/*   Updated: 2023/08/30 11:29:45 by tlakchai         ###   ########.fr       */
+/*   Updated: 2023/09/07 14:21:36 by tlakchai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *) haystack);
 	while (haystack[i] && i + count <= len)
 	{
-		if (haystack[i] == needle[0]
-			&& ft_strncmp(haystack + i, needle, count) == 0)
+		if (ft_strncmp(haystack + i, needle, count) == 0)
 			return ((char *) haystack + i);
 		i++;
 	}
