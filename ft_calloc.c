84d3a6e197e27791b9180f0a1e02ee7ded6a9262
meow <katesan945@gmail.com>
2023-09-24 20:06:43 +0700

    fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tlakchai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 18:38:25 by tlakchai          #+#    #+#             */
-/*   Updated: 2023/09/11 19:29:53 by tlakchai         ###   ########.fr       */
+/*   Updated: 2023/09/24 20:05:53 by tlakchai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,4 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(result, count * size);
 	return (result);
 }
+
