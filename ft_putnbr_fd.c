1f7370c8924609717503daf07d305e79b664603b
meow <katesan945@gmail.com>
2023-09-29 19:51:31 +0700

    huh

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tlakchai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 15:13:30 by tlakchai          #+#    #+#             */
-/*   Updated: 2023/09/29 19:51:11 by tlakchai         ###   ########.fr       */
+/*   Updated: 2023/09/29 19:51:23 by tlakchai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	pnbr_put(long n, int fd)
 	}
 	if (n >= 10)
 		pnbr_put(n / 10, fd);
-	pnbr_put((n % 10) + '0', fd);
+	ft_putchar_fd((n % 10) + '0', fd);
 }
 
 void	ft_putnbr_fd(int n, int fd)
