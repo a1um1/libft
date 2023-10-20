73ff78f9d67845554ca80454717ea468a634da35
meow <katesan945@gmail.com>
2023-10-20 17:06:47 +0700

    change to char * to own string type

--- libft.h
+++ libft.h
@@ -29,7 +29,7 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const string s);
 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
@@ -42,28 +42,28 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strchr(const string s, int c);
+char	*ft_strrchr(const string s, int c);
+char	*ft_strdup(const string s1);
+int		ft_strncmp(const string s1, const string s2, size_t n);
+size_t	ft_strlcat(string dst, const string src, size_t dstsize);
+size_t	ft_strlcpy(string dst, const string src, size_t dstsize);
+char	*ft_strnstr(const string haystack, const string needle, size_t len);
 
-int		ft_atoi(const char *str);
+int		ft_atoi(const string str);
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
+string  ft_substr(const string s, unsigned int start, size_t len);
+string  ft_strjoin(const string s1, const string s2);
+string  ft_strtrim(const string s1, const string set);
+string  *ft_split(const string s, char c);
 
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr_fd(string s, int fd);
 
-void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(string s, int fd);
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(const string s, char (*f)(unsigned int, char));
+void	ft_striteri(string s, void (*f)(unsigned int, string));
 char	*ft_itoa(int n);
 void	ft_putnbr_fd(int n, int fd);
 
