1d69496714851b29556640a037a288ba60d52dc2
A1UM1 <88581492+a1um1@users.noreply.github.com>
2023-11-16 23:53:28 +0700

    Update ft_strjoin.c

--- str/ft_strjoin.c
+++ str/ft_strjoin.c
@@ -14,25 +14,22 @@
 
 t_string	ft_strjoin(const t_string s1, const t_string s2)
 {
-	size_t		s1_size;
-	size_t		s2_size;
-	t_string	ps1;
-	t_string	ps2;
+	size_t		i;
+	size_t		x;
 	t_string	result;
 
 	if (!s1 || !s2)
 		return (NULL);
-	ps1 = s1;
-	ps2 = s2;
-	s1_size = ft_strlen(s1);
-	s2_size = ft_strlen(s2);
-	result = malloc((s1_size + s2_size + 1) * sizeof(char));
+	result = malloc((gnl_strlen(s1) + gnl_strlen(s2) + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
-	while (*s1)
-		*(result++) = *(ps1++);
-	while (*s2)
-		*(result++) = *(ps2++);
-	*result = 0;
-	return (result - (s1_size + s2_size));
+	i = 0;
+	x = 0;
+	while (s1[i] != '\0')
+		result[i++] = s1[x++];
+	x = 0;
+	while (s2[x] != '\0')
+		result[i++] = s2[x++];
+	result[i] = 0;
+	return (result);
 }
